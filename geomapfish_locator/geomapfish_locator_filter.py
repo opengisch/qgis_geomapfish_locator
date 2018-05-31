b204cd50b023b2940e8e1106711c4be080b6b4df
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-05-31 09:36:39 -0400

    fix typo and use qgis.PyQt.uic

--- geomapfish_locator/geomapfish_locator_filter.py
+++ geomapfish_locator/geomapfish_locator_filter.py
@@ -33,7 +33,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import QUrl, QUrlQuery, QByteArray
 from PyQt5.QtWidgets import QDialog
-from PyQt5.uic import loadUiType
+from qgis.PyQt.uic import loadUiType
 
 from qgis.core import Qgis, QgsMessageLog, QgsLocatorFilter, QgsLocatorResult, QgsApplication, \
     QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject, QgsGeometry, QgsWkbTypes
