738c7f4fab346fbf56f1a1be6aec1c25bafff3f7
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-06-11 11:04:24 -0400

    use try...except

--- geomapfish_locator/geomapfish_locator_filter.py
+++ geomapfish_locator/geomapfish_locator_filter.py
@@ -28,10 +28,11 @@
 import json
 import os
 import re
+import sys, traceback
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
-from PyQt5.QtCore import QUrl, QUrlQuery, QByteArray
+from PyQt5.QtCore import QUrl, QUrlQuery, QByteArray, QTimer, pyqtSlot
 from PyQt5.QtWidgets import QDialog
 from qgis.PyQt.uic import loadUiType
 
@@ -70,9 +71,9 @@ class GeomapfishLocatorFilter(QgsLocatorFilter):
         super().__init__()
         self.rubber_band = None
         self.settings = Settings()
-        self.reply = None
         self.iface = None
         self.map_canvas = None
+        self.current_timer = None
 
         # only get map_canvas on main thread, not when cloning
         if iface is not None:
@@ -125,80 +126,104 @@ class GeomapfishLocatorFilter(QgsLocatorFilter):
         self.resultFetched.emit(result)
         return
 
+    @pyqtSlot()
+    def clear_results(self):
+        if self.rubber_band:
+            self.rubber_band.reset(QgsWkbTypes.PointGeometry)
+        if self.current_timer is not None:
+            self.current_timer.timeout.disconnect(self.clear_results)
+            self.current_timer.stop()
+            self.current_timer.deleteLater()
+            self.current_timer = None
+
     def fetchResults(self, search, context, feedback):
-        self.dbg_info("start GMF locator search...")
-
-        if self.reply is not None and self.reply.isRunning():
-            self.reply.abort()
-
-        url = self.settings.value('geomapfish_url')
-
-        if url == "":
-            self.emit_bad_configuration()
-            return
-
-        params = {
-            'query': search,
-            'limit': str(self.settings.value('total_limit')),
-            'partitionlimit': str(self.settings.value('category_limit'))
-        }
-
-        if len(search) < 2:
-            return
-
-        headers = {b'User-Agent': self.USER_AGENT}
-        if self.settings.value('geomapfish_user') != '':
-            user = self.settings.value('geomapfish_user')
-            password = self.settings.value('geomapfish_pass')
-            auth_data = "{}:{}".format(user, password)
-            b64 = QByteArray(auth_data.encode()).toBase64()
-            headers[QByteArray('Authorization'.encode())] = QByteArray('Basic '.encode()) + b64
-
-        url = self.url_with_param(url, params)
-        self.dbg_info(url)
-
-        nam = NetworkAccessManager()
-        feedback.canceled.connect(nam.abort)
         try:
-            (response, content) = nam.request(url, headers=headers, blocking=True)
-            self.handle_response(response, content)
-        except RequestsExceptionUserAbort:
-            pass
-        except RequestsException as err:
-            self.emit_bad_configuration(str(err))
-            self.info(err)
+            self.dbg_info("start GMF locator search...")
+
+            url = self.settings.value('geomapfish_url')
+
+            if url == "":
+                self.emit_bad_configuration()
+                return
+
+            params = {
+                'query': search,
+                'limit': str(self.settings.value('total_limit')),
+                'partitionlimit': str(self.settings.value('category_limit'))
+            }
+
+            if len(search) < 2:
+                return
+
+            headers = {b'User-Agent': self.USER_AGENT}
+            if self.settings.value('geomapfish_user') != '':
+                user = self.settings.value('geomapfish_user')
+                password = self.settings.value('geomapfish_pass')
+                auth_data = "{}:{}".format(user, password)
+                b64 = QByteArray(auth_data.encode()).toBase64()
+                headers[QByteArray('Authorization'.encode())] = QByteArray('Basic '.encode()) + b64
+
+            url = self.url_with_param(url, params)
+            self.dbg_info(url)
+
+            nam = NetworkAccessManager()
+            feedback.canceled.connect(nam.abort)
+            try:
+                (response, content) = nam.request(url, headers=headers, blocking=True)
+                self.handle_response(response, content)
+            except RequestsExceptionUserAbort:
+                pass
+            except RequestsException as err:
+                self.emit_bad_configuration(str(err))
+                self.info(err)
+
+        except Exception as e:
+            self.info(str(e), Qgis.Critical)
+            #exc_type, exc_obj, exc_traceback = sys.exc_info()
+            #filename = os.path.split(exc_traceback.tb_frame.f_code.co_filename)[1]
+            #self.info('{} {} {}'.format(exc_type, filename, exc_traceback.tb_lineno), Qgis.Critical)
+            #self.info(traceback.print_exception(exc_type, exc_obj, exc_traceback), Qgis.Critical)
 
     def handle_response(self, response, content):
-        if response.status_code != 200:
-            self.info("Error with status code: {}".format(response.status_code))
-            return
+        try:
+            if response.status_code != 200:
+                self.info("Error with status code: {}".format(response.status_code))
+                return
 
-        data = json.loads(content.decode('utf-8'))
-        #self.dbg_info(data)
+            data = json.loads(content.decode('utf-8'))
+            #self.dbg_info(data)
 
-        features = data['features']
-        for f in features:
-            json_geom = json.dumps(f['geometry'])
-            ogr_geom = ogr.CreateGeometryFromJson(json_geom)
-            wkt = ogr_geom.ExportToWkt()
-            geometry = QgsGeometry.fromWkt(wkt)
-            self.dbg_info('---------')
-            self.dbg_info(QgsWkbTypes.geometryDisplayString(geometry.type()))
-            self.dbg_info(f.keys())
-            self.dbg_info('{} {}'.format(f['properties']['layer_name'], f['properties']['label']))
-            self.dbg_info(f['bbox'])
-            self.dbg_info(f['geometry'])
-            if geometry is None:
-                continue
-            result = QgsLocatorResult()
-            result.filter = self
-            result.displayString = f['properties']['label']
-            if Qgis.QGIS_VERSION_INT >= 30100:
-                result.group = self.beautify_group(f['properties']['layer_name'])
-            result.userData = geometry
-            self.resultFetched.emit(result)
+            features = data['features']
+            for f in features:
+                json_geom = json.dumps(f['geometry'])
+                ogr_geom = ogr.CreateGeometryFromJson(json_geom)
+                wkt = ogr_geom.ExportToWkt()
+                geometry = QgsGeometry.fromWkt(wkt)
+                self.dbg_info('---------')
+                self.dbg_info(QgsWkbTypes.geometryDisplayString(geometry.type()))
+                self.dbg_info(f.keys())
+                self.dbg_info('{} {}'.format(f['properties']['layer_name'], f['properties']['label']))
+                self.dbg_info(f['bbox'])
+                self.dbg_info(f['geometry'])
+                if geometry is None:
+                    continue
+                result = QgsLocatorResult()
+                result.filter = self
+                result.displayString = f['properties']['label']
+                if Qgis.QGIS_VERSION_INT >= 30100:
+                    result.group = self.beautify_group(f['properties']['layer_name'])
+                result.userData = geometry
+                self.resultFetched.emit(result)
+
+        except Exception as e:
+            self.info(str(e), Qgis.Critical)
+            #exc_type, exc_obj, exc_traceback = sys.exc_info()
+            #filename = os.path.split(exc_traceback.tb_frame.f_code.co_filename)[1]
+            #self.info('{} {} {}'.format(exc_type, filename, exc_traceback.tb_lineno), Qgis.Critical)
+            # self.info(traceback.print_exception(exc_type, exc_obj, exc_traceback), Qgis.Critical)
 
     def triggerResult(self, result):
+        self.clear_results()
         if result.userData == FilterNotConfigured:
             self.openConfigWidget()
             if self.iface and hasattr(self.iface, 'invalidateLocatorResults'):
@@ -222,6 +247,11 @@ class GeomapfishLocatorFilter(QgsLocatorFilter):
         rect.scale(1.5)
         self.map_canvas.setExtent(rect)
         self.map_canvas.refresh()
+
+        self.current_timer = QTimer()
+        self.current_timer.timeout.connect(self.clear_results)
+        self.current_timer.setSingleShot(True)
+        self.current_timer.start(10000)
 
     def beautify_group(self, group) -> str:
         if self.settings.value("remove_leading_digits"):
